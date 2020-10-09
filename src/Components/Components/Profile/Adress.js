014b5d145fdb47c368dfd0d852ac5338571ffc13
yesminee <43929137+yesminee@users.noreply.github.com>
2020-10-09 08:13:38 +0100

    Update Adress.js

--- src/Components/Components/Profile/Adress.js
+++ src/Components/Components/Profile/Adress.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../App.css";
 
-export const adress = () => {
+const adress = () => {
   return <div className="text">23Rue Maillorca Riadh El Andalous 2058 Ariana Tunis</div>;
 };
 export default adress;
